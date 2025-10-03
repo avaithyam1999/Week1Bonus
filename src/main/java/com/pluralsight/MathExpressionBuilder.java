4062fe9f3d3a12441c07e0f18d34f3b6eb1d9708
AJ V <avaithyam@yahoo.com>
2025-10-03 11:25:50 -0400

    exercise 1 init commit

---
+++ src/main/java/com/pluralsight/MathExpressionBuilder.java
@@ -0,0 +1,28 @@
+package com.pluralsight;
+
+public class MathExpressionBuilder {
+    public static void main(String[] args) {
+        int x = 12;
+        int y = 7;
+        int z = 2;
+        double w = 1.8;
+
+        double simpleCalc = x + y * z - w;
+        double pemdasCalc = (x + y) * z - w;
+
+        double difference = Math.abs(simpleCalc-pemdasCalc);
+
+
+        System.out.printf("""
+                === Mathematical Expression Builder ===
+                Original expression: %d + %d * %d - %f
+                Direct Calculation: %f
+                
+                With parentheses: (%d + %d) * %d - %f
+                PEMDAS Calculation: %f
+                
+                Difference: %d
+                
+                """,x,y,z,w,simpleCalc,x,y,z,w,pemdasCalc,difference);
+    }
+}
